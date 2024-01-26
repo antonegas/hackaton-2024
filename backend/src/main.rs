5f6a4d23547c10583c7e2445cfbc8a972128c6d9
the-JS-hater <mnordberg01@gmail.com>
2024-01-26 15:06:20 +0100

    added a lil example code comment

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -48,3 +48,12 @@ fn handle_connection(mut stream: TcpStream) {
             }
     }
 }
+
+//EXAMPLE of returning a HTML response
+//Look into finding HTML's from frontend folder
+    //let status_line = "HTTP/1.1 200 OK";
+  //  let contents = fs::read_to_string("hello.html").unwrap();
+  //  let length = contents.len();
+
+  //  let response =
+  //      format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");

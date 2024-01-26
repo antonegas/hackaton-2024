d0149411b0876e89cc8d734bda1cc1b23a8e60e4
antonegas <32789589+antonegas@users.noreply.github.com>
2024-01-26 19:01:27 +0100

    frontend setup

--- backend/target/debug/deps/serverTesting-4288c26158bdbe6d.d
+++
@@ -1,5 +0,0 @@
-/home/morgan/rust/server-testing/target/debug/deps/serverTesting-4288c26158bdbe6d: src/main.rs
-
-/home/morgan/rust/server-testing/target/debug/deps/serverTesting-4288c26158bdbe6d.d: src/main.rs
-
-src/main.rs:
--- backend/target/debug/serverTesting.d
+++
@@ -1,1 +0,0 @@
-/home/morgan/rust/server-testing/target/debug/serverTesting: /home/morgan/rust/server-testing/src/main.rs

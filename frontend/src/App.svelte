5da25dfc4ce7387dc5b87f14f6853678e46ce802
antonegas <32789589+antonegas@users.noreply.github.com>
2024-01-27 18:26:28 +0100

    LAN host redirect

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -87,25 +87,4 @@
     color: var(--primary-color);
     animation: 2s ease-in-out 0s infinite swag;
   }
-
-  @keyframes swag {
-  0% {
-    transform: translate(0, 0);
-    text-shadow: 0.125rem 0.125rem 0 var(--accent-color),
-    0.25rem 0.25rem 0 var(--accent-color),
-    0.375rem 0.375rem 0 var(--accent-color),
-    0.5rem 0.5rem 0 var(--accent-color);
-  }
-  50% {
-    transform: translate(0.5rem, 0.5rem);
-    text-shadow: 0 0 0 var(--primary-color);
-  }
-  100% {
-    transform: translate(0, 0);
-    text-shadow: 0.125rem 0.125rem 0 var(--accent-color),
-    0.25rem 0.25rem 0 var(--accent-color),
-    0.375rem 0.375rem 0 var(--accent-color),
-    0.5rem 0.5rem 0 var(--accent-color);
-  }
-}
 </style>
--- frontend/src/lib/SignIn.svelte
+++ frontend/src/lib/SignIn.svelte
@@ -2,7 +2,7 @@
   import ActionButton from "./ActionButton.svelte";
 
   function authWithDiscord() {
-    window.location.replace("https://discord.com/api/oauth2/authorize?client_id=1200515855537160323&response_type=token&redirect_uri=http%3A%2F%2Flocalhost%3A5173%2F&scope=identify")
+    window.location.replace("https://discord.com/api/oauth2/authorize?client_id=1200515855537160323&response_type=token&redirect_uri=http%3A%2F%2F192.168.168.218%3A5173%2F&scope=identify")
   }
 </script>
 

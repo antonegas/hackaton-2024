e4da8f028578621d1bde261e4afd51596e073dce
antonegas <32789589+antonegas@users.noreply.github.com>
2024-01-27 15:00:21 +0100

    party create?

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -6,6 +6,7 @@
   import Counter from "./lib/Counter.svelte";
   import SignIn from "./lib/SignIn.svelte";
   import UserInfo from "./lib/UserInfo.svelte";
+  import PartyJoin from "./lib/PartyJoin.svelte";
 
   let signedIn: boolean = localStorage.getItem("signedIn") === "true";
 
@@ -45,39 +46,18 @@
   });
 </script>
 
-<!--<h1>Ball Pit</h1>
+<h1>Ball Pit</h1>
 
-{#if !signedIn}
-  <SignIn />
-{:else}
-  <UserInfo />
-{/if}
+<div class="info-wrapper">
+  {#if !signedIn}
+    <SignIn />
+  {:else}
+    <UserInfo />
+    <PartyJoin />
+  {/if}
+</div>
 
 <!-- <Game1 /> -->
-
-<!-- <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
-</main> -->
 
 <style>
   .logo {
